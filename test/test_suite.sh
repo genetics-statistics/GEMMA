35405450385ded8797a9a6388e6e2801381f2894
Pjotr Prins <pjotr.public01@thebird.nl>
2017-07-09 08:07:46 +0000

    Test: check result

--- test/test_suite.sh
+++ test/test_suite.sh
@@ -2,10 +2,13 @@
 
 gemma=../bin/gemma
 
-testCenteredRelatednessMatrix() {
+testCenteredRelatednessMatrixK() {
     $gemma -g ../example/mouse_hs1940.geno.txt.gz -p ../example/mouse_hs1940.pheno.txt \
-                 -a ../example/mouse_hs1940.anno.txt -gk -o mouse_hs1940
+           -a ../example/mouse_hs1940.anno.txt -gk -o mouse_hs1940
     assertEquals "3763600" `wc -w < output/mouse_hs1940.cXX.txt`
+    # assertEquals "15f680c" `md5sum < output/mouse_hs1940.cXX.txt | head -c 7`
+    assertEquals "0.335" `head -c 5 output/mouse_hs1940.cXX.txt`
+    assertEquals "29.691" `awk '{s+=substr($1,0,6)}END{print s}' output/mouse_hs1940.cXX.txt`
 }
 
 shunit2=`which shunit2`
