cfcd91563e90e0492d93ab273e72fe73a97af94f
Pjotr Prins <pjotr.public01@thebird.nl>
2017-08-13 05:46:50 +0000

    Fixes https://github.com/genetics-statistics/GEMMA/issues/64 debug.h: replaced assert_fail with enforce_fail

--- src/debug.h
+++ src/debug.h
@@ -1,6 +1,9 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+#include <assert.h>
+#include <iostream>
+
 // enforce works like assert but also when NDEBUG is set (i.e., it
 // always works). enforce_msg prints message instead of expr
 
@@ -11,26 +14,29 @@
 #endif
 
 /* This prints an "Assertion failed" message and aborts.  */
-extern "C" void __assert_fail(const char *__assertion, const char *__file,
-                              unsigned int __line,
-                              const char *__function) __THROW
-    __attribute__((__noreturn__));
+inline void __enforce_fail(const char *__assertion, const char *__file,
+                    unsigned int __line,
+                    const char *__function)
+{
+  std::cout << "ERROR: Enforce failed for " << __assertion << " in " << __file << " at line " << __line << " in " << __function << std::endl;
+  exit(1);
+}
 
 #define __ASSERT_FUNCTION __PRETTY_FUNCTION__
 
 #define enforce(expr)                                                          \
   ((expr)                                                                      \
        ? __ASSERT_VOID_CAST(0)                                                 \
-       : __assert_fail(__STRING(expr), __FILE__, __LINE__, __ASSERT_FUNCTION))
+       : __enforce_fail(__STRING(expr), __FILE__, __LINE__, __ASSERT_FUNCTION))
 
 #define enforce_msg(expr, msg)                                                 \
   ((expr) ? __ASSERT_VOID_CAST(0)                                              \
-          : __assert_fail(msg, __FILE__, __LINE__, __ASSERT_FUNCTION))
+          : __enforce_fail(msg, __FILE__, __LINE__, __ASSERT_FUNCTION))
 
 #define enforce_str(expr, msg)                                                 \
   ((expr)                                                                      \
        ? __ASSERT_VOID_CAST(0)                                                 \
-       : __assert_fail((msg).c_str(), __FILE__, __LINE__, __ASSERT_FUNCTION))
+       : __enforce_fail((msg).c_str(), __FILE__, __LINE__, __ASSERT_FUNCTION))
 
 // Helpers to create a unique varname per MACRO
 #define COMBINE1(X, Y) X##Y
@@ -40,7 +46,7 @@ extern "C" void __assert_fail(const char *__assertion, const char *__file,
   auto COMBINE(res, __LINE__) = (expr);                                        \
   (COMBINE(res, __LINE__) == 0                                                 \
        ? __ASSERT_VOID_CAST(0)                                                 \
-       : __assert_fail(gsl_strerror(COMBINE(res, __LINE__)), __FILE__,         \
-                       __LINE__, __ASSERT_FUNCTION))
+       : __enforce_fail(gsl_strerror(COMBINE(res, __LINE__)), __FILE__,         \
+                        __LINE__, __ASSERT_FUNCTION))
 
 #endif
