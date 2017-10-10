362f77ebdd5a7eeda6244c37f353016572aabe3f
Pjotr Prins <pjotr.public01@thebird.nl>
2017-10-10 10:02:28 +0000

    Removing warnings on debug.h assignment

--- src/debug.cpp
+++ src/debug.cpp
@@ -23,8 +23,8 @@ void do_validate_K(const gsl_matrix *K, bool do_check, bool strict, const char *
   if (do_check) {
     // debug_msg("Validating K");
     auto eigenvalues = getEigenValues(K);
-    uint count_small;
-    if (count_small = count_small_values(eigenvalues,EIGEN_MINVALUE)>1) {
+    const uint count_small = count_small_values(eigenvalues,EIGEN_MINVALUE);
+    if (count_small>1) {
       std::string msg = "K has ";
       msg += std::to_string(count_small);
       msg += " eigenvalues close to zero";
@@ -38,7 +38,7 @@ void do_validate_K(const gsl_matrix *K, bool do_check, bool strict, const char *
     if (negative_values = has_negative_values_but_one(eigenvalues)) {
       warning_at_msg(__file,__line,"K has more than one negative eigenvalues!");
     }
-    if (count_small>=0 && negative_values && !isMatrixPositiveDefinite(K))
+    if (count_small>0 && negative_values && !isMatrixPositiveDefinite(K))
       fail_at_msg(strict,__file,__line,"K is not positive definite!");
     gsl_vector_free(eigenvalues);
   }
