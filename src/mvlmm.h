c18db6d6c9ddeb14e42e00046d900671f25114ba
jb621-star <65730803+jb621-star@users.noreply.github.com>
2024-10-28 13:08:09 -0400

    Update mvlmm.h
    Got rid of sigmasq mentions where they aren't needed.

--- src/mvlmm.h
+++ src/mvlmm.h
@@ -80,9 +80,9 @@ public:
   // Main functions
   void CopyFromParam(PARAM &cPar);
   void CopyToParam(PARAM &cPar);
-  void AnalyzeBimbam(const gsl_matrix *U, const gsl_vector *eval, const gsl_matrix *sigmasq,
+  void AnalyzeBimbam(const gsl_matrix *U, const gsl_vector *eval,
                      const gsl_matrix *UtW, const gsl_matrix *UtY);
-  void AnalyzePlink(const gsl_matrix *U, const gsl_vector *eval, const gsl_matrix *sigmasq,
+  void AnalyzePlink(const gsl_matrix *U, const gsl_vector *eval,
                     const gsl_matrix *UtW, const gsl_matrix *UtY);
   void Analyzebgen(const gsl_matrix *U, const gsl_vector *eval,
                    const gsl_matrix *UtW, const gsl_matrix *UtY);
@@ -95,7 +95,7 @@ public:
   void WriteFiles();
 };
 
-void CalcMvLmmVgVeBeta(const gsl_matrix *U, const gsl_vector *eval, const gsl_matrix *sigmasq, const gsl_matrix *UtW,
+void CalcMvLmmVgVeBeta(const gsl_matrix *U, const gsl_vector *eval, const gsl_matrix *UtW,
                        const gsl_matrix *UtY, const size_t em_iter,
                        const size_t nr_iter, const double em_prec,
                        const double nr_prec, const double l_min,
